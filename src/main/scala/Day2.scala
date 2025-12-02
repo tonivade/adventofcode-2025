94d1781c5d3c9e45ffe90d6567623810003b763d
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-02 16:47:17 +0100

    mal: day2 part1

---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,14 @@
+package day2
+
+import scala.io.Source
+
+// https://adventofcode.com/2025/day/2
+object Day2:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day2.txt").getLines().mkString("\n")
+  println(Day2.part1(input))
+  println(Day2.part2(input))
+
---
+++ src/test/scala/Day2Suite.scala
@@ -0,0 +1,14 @@
+package day2
+
+import Day2._
+
+class Day2Suite extends munit.FunSuite:
+
+  val input = """""".stripMargin
+
+  test("Day2 part1".ignore):
+    assertEquals(part1(input), 1)
+
+  test("Day2 part2".ignore):
+    assertEquals(part2(input), 1)
+
