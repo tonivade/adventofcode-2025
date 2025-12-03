3dfcd4df8a01c9ce7d3975289b71e1ab2e390f32
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-04 00:48:20 +0100

    day3 part1 scafolding

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,14 @@
+package day3
+
+import scala.io.Source
+
+// https://adventofcode.com/2025/day/3
+object Day3:
+  def part1(input: String): Int = ???
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day3.txt").getLines().mkString("\n")
+  println(Day3.part1(input))
+  println(Day3.part2(input))
+
---
+++ src/test/scala/Day3Suite.scala
@@ -0,0 +1,17 @@
+package day3
+
+import Day3._
+
+class Day3Suite extends munit.FunSuite:
+
+  val input = """987654321111111
+                |811111111111119
+                |234234234234278
+                |818181911112111""".stripMargin
+
+  test("Day3 part1".ignore):
+    assertEquals(part1(input), 1)
+
+  test("Day3 part2".ignore):
+    assertEquals(part2(input), 1)
+
