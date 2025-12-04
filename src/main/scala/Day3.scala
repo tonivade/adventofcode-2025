315ebf2338c4dae9f2fc35cb994a78768b7f612b
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-04 23:54:42 +0100

    scala: day3 part1

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -4,7 +4,22 @@ import scala.io.Source
 
 // https://adventofcode.com/2025/day/3
 object Day3:
-  def part1(input: String): Int = ???
+  case class Bat(jolts: Int, position: Int)
+
+  def active(bank: Seq[Int]): Int =
+    var all = for {
+      i <- 0 until bank.length
+      j <- (i + 1) until bank.length
+    } yield (bank(i), bank(j))
+    all.map:
+        case (i, j) => (i * 10) + j
+      .max
+
+  def part1(input: String): Int = 
+    input.split("\n")
+      .map(_.map(_.toInt).map(_ - '0'.toInt))
+      .map(active)
+      .sum
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day3Suite.scala
+++ src/test/scala/Day3Suite.scala
@@ -9,8 +9,8 @@ class Day3Suite extends munit.FunSuite:
                 |234234234234278
                 |818181911112111""".stripMargin
 
-  test("Day3 part1".ignore):
-    assertEquals(part1(input), 1)
+  test("Day3 part1"):
+    assertEquals(part1(input), 357)
 
   test("Day3 part2".ignore):
     assertEquals(part2(input), 1)
