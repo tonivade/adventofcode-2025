159f3c9cfce4fc760e6bef9b47436a85422582f8
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-06 10:50:00 +0100

    scala: day6 part2

---
+++ src/main/scala/Day6.scala
@@ -0,0 +1,34 @@
+package day6
+
+import scala.io.Source
+import aoc.timed
+
+// https://adventofcode.com/2025/day/6
+object Day6:
+  def solve(input: Array[String]): Long =
+    input.foldRight(("", 0L)):
+        case ("+", (_, acc)) => ("+", 0L)
+        case ("*", (_, acc)) => ("*", 1L)
+        case (a, ("+", acc)) => ("+", acc + a.toLong)
+        case (a, ("*", acc)) => ("*", acc * a.toLong)
+      ._2
+
+  def part1(input: String): Long = 
+    input.split("\n")
+      .map(_.trim)
+      .map(_.split("\\s+"))
+      .flatMap(_.zipWithIndex)
+      .groupMap(_._2)(_._1)
+      .values
+      .map(solve)
+      .sum
+
+  def part2(input: String): Int = ???
+
+@main def main: Unit =
+  val input = Source.fromFile("input/day6.txt").getLines().mkString("\n")
+  timed():
+    println(s"Part 1: ${Day6.part1(input)}")
+  timed():
+    println(s"Part 2: ${Day6.part2(input)}")
+
---
+++ src/test/scala/Day6Suite.scala
@@ -0,0 +1,17 @@
+package day6
+
+import Day6._
+
+class Day6Suite extends munit.FunSuite:
+
+  val input = """123 328  51 64 
+                | 45 64  387 23 
+                |  6 98  215 314
+                |*   +   *   +  """.stripMargin
+
+  test("Day6 part1"):
+    assertEquals(part1(input), 4277556L)
+
+  test("Day6 part2".ignore):
+    assertEquals(part2(input), 1)
+
