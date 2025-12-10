a90b820ab42818e5d9b4d958cc5194e8ed5f3f22
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-10 19:32:44 +0100

    scala: day10 part1 WIP

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -6,7 +6,21 @@ import aoc.timed
 // https://adventofcode.com/2025/day/10
 object Day10:
 
-  case class Machine(lights: List[Boolean], buttons: List[List[Int]])
+  case class Machine(
+    currentLights: List[Boolean], 
+    lightsPattern: List[Boolean], 
+    buttons: List[List[Int]]):
+      def isOn: Boolean = currentLights == lightsPattern
+      def turnOn: Machine =
+        buttons.zipWithIndex.map(_._2).permutations.foldLeft(this):
+          case (current, bs) => bs.foldLeft(current):
+            case (m, b) if !m.isOn => m.click(b)
+            case (m, _) => m
+
+      def click(button: Int): Machine =
+        val nextLights = buttons(button).foldLeft(currentLights):
+          case (current, b) => current.updated(b, !current(b))
+        Machine(nextLights, lightsPattern, buttons)
 
   def part1(input: String): Int = 
     val regex = """^\[([.#]+)\]\s+((?:\([\d,]+\)\s*)+)\{([\d,]+)\}$""".r
@@ -15,8 +29,10 @@ object Day10:
       .map:
         case regex(lights, buttons, _) => 
           Machine(
+            lights.trim.map(_ => false).toList,
             lights.trim.map(x => x == '#').toList, 
             buttons.trim.split(" ").map(_.drop(1).dropRight(1).split(",").map(_.toInt).toList).toList)
+      .map(_.turnOn)
       .foreach(println)
 
     0
