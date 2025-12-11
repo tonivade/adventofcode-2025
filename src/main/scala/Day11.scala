47b317eeb7367c0ff7295baeb6c5bbfeba75a37e
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-11 22:05:36 +0100

    scala: day11 part2 WIP

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -5,33 +5,39 @@ import aoc.timed
 
 // https://adventofcode.com/2025/day/11
 object Day11:
-  case class Node(name: String, distance: Int) extends Comparable[Node]:
-    override def compareTo(other: Node): Int = this.distance - other.distance
 
-  def findAllPaths(graph: Map[String, List[String]]): List[List[String]] =
-    def dfs(current: String, visited: Set[String], path: List[String]): List[List[String]] =
+  def findAllPaths(graph: Map[String, List[String]], start: String): List[Set[String]] =
+    def dfs(current: String, visited: Set[String], path: Set[String]): List[Set[String]] =
       // If we reach the end, return the completed path
       if (current == "out") return List(path)
       // Otherwise explore neighbors
       graph.getOrElse(current, Nil).flatMap: neighbor =>
         // Avoid revisiting nodes to prevent cycles
         if (!visited.contains(neighbor))
-          dfs(neighbor, visited + neighbor, path :+ neighbor)
+          dfs(neighbor, visited + neighbor, path + neighbor)
         else
           Nil
 
-    dfs("you", Set("you"), List("you"))
+    dfs(start, Set(start), Set(start))
 
-  def part1(input: String): Int = 
-    val servers = input
+  def parse(input: String): Map[String, List[String]] =
+    input
       .linesIterator
       .map(_.split(":")).map:
         case Array(name, output) => name -> output.trim().split(" ").toList
       .toMap
 
-    findAllPaths(servers + ("out" -> List.empty)).size
+  def part1(input: String): Int = 
+    val servers = parse(input)
 
-  def part2(input: String): Int = ???
+    findAllPaths(servers + ("out" -> List.empty), "you").size
+
+  def part2(input: String): Int = 
+    val servers = parse(input)
+
+    findAllPaths(servers + ("out" -> List.empty), "svr").filter:
+        path => path.contains("dac") && path.contains("fft")
+      .size
 
 @main def main: Unit =
   val input = Source.fromFile("input/day11.txt").getLines().mkString("\n")
--- src/test/scala/Day11Suite.scala
+++ src/test/scala/Day11Suite.scala
@@ -4,20 +4,34 @@ import Day11._
 
 class Day11Suite extends munit.FunSuite:
 
-  val input = """aaa: you hhh
-                |you: bbb ccc
-                |bbb: ddd eee
-                |ccc: ddd eee fff
-                |ddd: ggg
-                |eee: out
-                |fff: out
-                |ggg: out
-                |hhh: ccc fff iii
-                |iii: out""".stripMargin
+  val input1 = """aaa: you hhh
+                 |you: bbb ccc
+                 |bbb: ddd eee
+                 |ccc: ddd eee fff
+                 |ddd: ggg
+                 |eee: out
+                 |fff: out
+                 |ggg: out
+                 |hhh: ccc fff iii
+                 |iii: out""".stripMargin
+
+  val input2 = """svr: aaa bbb
+                 |aaa: fft
+                 |fft: ccc
+                 |bbb: tty
+                 |tty: ccc
+                 |ccc: ddd eee
+                 |ddd: hub
+                 |hub: fff
+                 |eee: dac
+                 |dac: fff
+                 |fff: ggg hhh
+                 |ggg: out
+                 |hhh: out""".stripMargin
 
   test("Day11 part1"):
-    assertEquals(part1(input), 5)
+    assertEquals(part1(input1), 5)
 
-  test("Day11 part2".ignore):
-    assertEquals(part2(input), 1)
+  test("Day11 part2"):
+    assertEquals(part2(input2), 2)
 
