aa37809594bca734d9bc6712d2ad29cf06249914
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-11 21:40:46 +0100

    scala: day11 part1

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -2,49 +2,35 @@ package day11
 
 import scala.io.Source
 import aoc.timed
-import scala.collection.mutable.{Map => MutableMap}
-import scala.collection.mutable.PriorityQueue
 
 // https://adventofcode.com/2025/day/11
 object Day11:
-  case class Server(output: List[String])
-
   case class Node(name: String, distance: Int) extends Comparable[Node]:
     override def compareTo(other: Node): Int = this.distance - other.distance
 
-  def search(graph: Map[String, Server]): Int =
-    val distances: MutableMap[String, Int] =
-      MutableMap(graph.keys.map(_ -> Int.MaxValue).toSeq: _*)
+  def findAllPaths(graph: Map[String, List[String]]): List[List[String]] =
+    def dfs(current: String, visited: Set[String], path: List[String]): List[List[String]] =
+      // If we reach the end, return the completed path
+      if (current == "out") return List(path)
+      // Otherwise explore neighbors
+      graph.getOrElse(current, Nil).flatMap: neighbor =>
+        // Avoid revisiting nodes to prevent cycles
+        if (!visited.contains(neighbor))
+          dfs(neighbor, visited + neighbor, path :+ neighbor)
+        else
+          Nil
 
-    distances("you") = 0
-
-    val pq = PriorityQueue(Node("you", 0))
-
-    while (pq.nonEmpty)
-      val Node(current, distance) = pq.dequeue()
-
-      if (distance == distances(current))
-        for (neighbor <- graph(current).output)
-          val newDist = distance + 1
-          if (newDist < distances(neighbor))
-            distances(neighbor) = newDist
-            pq.enqueue(Node(neighbor, newDist))
-
-    distances.foreach(println)
-
-    0
+    dfs("you", Set("you"), List("you"))
 
   def part1(input: String): Int = 
     val servers = input
       .linesIterator
       .map(_.split(":")).map:
-        case Array(name, output) => name -> new Server(output.trim().split(" ").toList)
+        case Array(name, output) => name -> output.trim().split(" ").toList
       .toMap
 
-    search(servers + ("out" -> Server(List.empty)))
+    findAllPaths(servers + ("out" -> List.empty)).size
 
-
-    0
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day11Suite.scala
+++ src/test/scala/Day11Suite.scala
@@ -16,7 +16,7 @@ class Day11Suite extends munit.FunSuite:
                 |iii: out""".stripMargin
 
   test("Day11 part1"):
-    assertEquals(part1(input), 1)
+    assertEquals(part1(input), 5)
 
   test("Day11 part2".ignore):
     assertEquals(part2(input), 1)
