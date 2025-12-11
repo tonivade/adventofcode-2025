724d5cd32483bf8cefd26a6f36e9617cb8e169b5
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-11 21:17:17 +0100

    scala: Day11 part1 WIP

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -2,8 +2,8 @@ package day11
 
 import scala.io.Source
 import aoc.timed
-import java.util.PriorityQueue
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.{Map => MutableMap}
+import scala.collection.mutable.PriorityQueue
 
 // https://adventofcode.com/2025/day/11
 object Day11:
@@ -13,31 +13,24 @@ object Day11:
     override def compareTo(other: Node): Int = this.distance - other.distance
 
   def search(graph: Map[String, Server]): Int =
-    val queue = PriorityQueue[Node]()
-    graph.keys.foreach:
-      n => queue.add(new Node(n, if n == "you" then 0 else Int.MaxValue))
+    val distances: MutableMap[String, Int] =
+      MutableMap(graph.keys.map(_ -> Int.MaxValue).toSeq: _*)
 
-    val index = HashMap.empty[String, Node]
-    queue.forEach:
-      n => index.put(n.name, n)
+    distances("you") = 0
 
-    while(!queue.isEmpty) {
-      val current = queue.remove()
-      val edges = graph(current.name)
+    val pq = PriorityQueue(Node("you", 0))
 
-      for (edge <- edges.output) {
-        val distance = 1 + current.distance
-        val other = index(edge)
-        if (distance < other.distance) {
-          val newNode = Node(other.name, distance)
-          queue.remove(other)
-          queue.add(newNode)
-          index.put(other.name, newNode)
-        }
-      }
-    }
+    while (pq.nonEmpty)
+      val Node(current, distance) = pq.dequeue()
 
-    index.foreach(println)
+      if (distance == distances(current))
+        for (neighbor <- graph(current).output)
+          val newDist = distance + 1
+          if (newDist < distances(neighbor))
+            distances(neighbor) = newDist
+            pq.enqueue(Node(neighbor, newDist))
+
+    distances.foreach(println)
 
     0
 
