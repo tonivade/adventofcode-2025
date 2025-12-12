cc6161bffaa14c9ab80817e1dc45f9509f0fee50
Antonio Muñoz <antoniogmc@gmail.com>
2025-12-12 13:41:23 +0100

    scala: day12 part1 WIP

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -5,7 +5,41 @@ import aoc.timed
 
 // https://adventofcode.com/2025/day/12
 object Day12:
-  def part1(input: String): Int = ???
+
+  case class Tree(width: Int, height: Int, presents: List[Int]):
+    def totalArea: Int = width * height
+    def requiredArea(allPresents: Array[Present]): Int = 
+      presents.zipWithIndex.map:
+        (times, position) => allPresents(position).area * times
+      .sum
+
+  case class Present(number: Int, shape: List[List[Int]]):
+    def area: Int = shape.flatMap(identity).count(_ == 1)
+
+  def part1(input: String): Int = 
+    val groups = input.split("\n\n")
+
+    val presents = groups.dropRight(1).map:
+      present => 
+        val number = present.split("\n").head.split(":").head.toInt
+        val shape = present.split("\n").tail.map(_.map(ch => if ch == '#' then 1 else 0).toList).toList
+        Present(number, shape)
+
+    val trees = groups.last.linesIterator.map:
+        line => 
+          val Array(dimensions, presents) = line.split(":")
+          val Array(width, height) = dimensions.split("x")
+          Tree(width.toInt, height.toInt, presents.trim.split(" ").map(_.toInt).toList)
+      .toList
+
+    presents.foreach(println)
+    trees.foreach(println)
+
+    trees.count:
+        tree => 
+          println(s"${tree.totalArea} vs ${tree.requiredArea(presents)}")
+          tree.totalArea >= tree.requiredArea(presents);
+
   def part2(input: String): Int = ???
 
 @main def main: Unit =
--- src/test/scala/Day12Suite.scala
+++ src/test/scala/Day12Suite.scala
@@ -38,8 +38,8 @@ class Day12Suite extends munit.FunSuite:
                 |12x5: 1 0 1 0 2 2
                 |12x5: 1 0 1 0 3 2""".stripMargin
 
-  test("Day12 part1".ignore):
-    assertEquals(part1(input), 1)
+  test("Day12 part1"):
+    assertEquals(part1(input), 2)
 
   test("Day12 part2".ignore):
     assertEquals(part2(input), 1)
